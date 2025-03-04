cb0abaa00f5446358613fcb6dc01ffa9a768e6c6
cho4u4o <cho4u4o@gmail.com>
2025-03-04 12:46:36 +0900

    fix : fix route

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     children: [HomeRoutes],
   },
-  { path: "/auth", element: <AuthPage /> },
+  { path: "auth", element: <AuthPage /> },
   { path: "*", element: <ErrorPage /> },
 ]);
 
