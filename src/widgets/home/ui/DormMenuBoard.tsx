09be758fa8e7495520964141e4744d62087e4b2b
cho4u4o <cho4u4o@gmail.com>
2025-03-07 01:20:55 +0900

    fix : fix growing height

--- src/widgets/home/ui/DormMenuBoard.tsx
+++ src/widgets/home/ui/DormMenuBoard.tsx
@@ -8,7 +8,7 @@ import { TimeSelector } from "~/widgets/home/ui";
 export default function DormMenuBoard() {
   const [time, setTime] = useState<Time>("점심");
   return (
-    <div className="flex h-[75%] w-full flex-col gap-x-4 md:flex-row 2xl:h-130">
+    <div className="flex h-[75%] w-full flex-shrink-0 flex-col gap-x-4 md:flex-row 2xl:h-130">
       <TimeSelector selected={time} setSelected={setTime} />
       <div className="rounded-normal flex h-full w-full flex-1 gap-x-5 bg-white p-5">
         <MenuView time={time} />
