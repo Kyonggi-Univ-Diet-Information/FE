76f8348e7482af549b396267bc87c2694991756f
cho4u4o <cho4u4o@gmail.com>
2025-03-07 01:14:05 +0900

    refactor : remove '0' while formatting date

--- src/shared/utils/formatDate.ts
+++ src/shared/utils/formatDate.ts
@@ -3,6 +3,10 @@ export const formatDate = (date: Date) => {
 };
 
 export const formatDatefromString = (date: string) => {
-  const [year, month, day] = date.split(".");
+  // eslint-disable-next-line prefer-const
+  let [year, month, day] = date.split(".");
+  if (month[0] === "0") month = month.slice(1);
+  if (day[0] === "0") day = day.slice(1);
+
   return `${year}년 ${month}월 ${day}일`;
 };
