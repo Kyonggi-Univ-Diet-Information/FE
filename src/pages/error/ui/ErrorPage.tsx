fd619d6f593251afd2591cde4fca7a2442a6c589
cho4u4o <cho4u4o@gmail.com>
2025-03-15 20:29:58 +0900

    fix : add error page & loading page

--- src/pages/error/ui/ErrorPage.tsx
+++ src/pages/error/ui/ErrorPage.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="grid h-screen w-full place-items-center">
+      <div className="flex flex-col gap-y-2">
+        <span className="text-lg font-semibold">404 Not Found</span>
+        <button
+          className="hover:bg-primary rounded-normal border-header-border h-[50px] w-full cursor-pointer border-[1px] bg-white px-4 text-black transition-all duration-300 hover:text-white"
+          onClick={() => (window.location.href = "https://www.kiryong.site/")}
+        >
+          홈으로 돌아가기
+        </button>
+      </div>
+    </div>
+  );
 }
