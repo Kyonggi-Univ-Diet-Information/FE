381ba2505f72cd6f7bfef067b7255f15a3613d8f
cho4u4o <cho4u4o@gmail.com>
2025-03-15 18:30:39 +0900

    design : develop login page design by add background image

--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -1,34 +1,27 @@
-import { Kakao, KaKaoSquare } from "~/assets";
+import { Kakao } from "~/assets";
+import School from "~/assets/kyonggi-univ.webp";
 
 export default function LoginPage() {
   return (
-    <div className="bg-primary/40 grid h-full w-full place-items-center overflow-hidden">
-      <div className="flex h-fit w-[75%] flex-col gap-y-4 md:w-120 lg:w-180">
-        <p className="w-full text-2xl font-medium">
+    <div
+      className="bg-primary/10 grid size-full place-items-center overflow-hidden bg-cover bg-center"
+      style={{ backgroundImage: `url("${School}")` }}
+    >
+      <div className="rounded-normal border-header-border relative flex h-fit w-[75%] flex-col gap-y-4 border-[1px] bg-white/60 p-10 md:w-120 lg:w-180">
+        <p className="md:font-2xl z-10 w-full text-xl font-medium">
           <b>카카오 로그인</b> 후 <br />
           <b>리뷰 기능</b>을 이용해보세요!
         </p>
-        <div className="flex flex-col gap-4 md:flex-row">
-          <div
-            className="rounded-normal h-40 w-full bg-cover bg-center"
-            style={{ backgroundImage: `url(${KaKaoSquare})` }}
-          />
-          <div
-            className="rounded-normal h-40 w-full bg-cover bg-center"
-            style={{ backgroundImage: `url(${KaKaoSquare})` }}
-          />
-        </div>
-        <div className="flex w-full justify-center">
-          <button
-            className="rounded-normal text-dark flex h-[50px] w-full cursor-pointer items-center justify-center bg-[#fee500] text-lg text-black transition-all duration-300 hover:bg-[#e8d102] md:w-100"
-            onClick={() => {
-              window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${import.meta.env.VITE_KAKAO_REST_API_KEY}&redirect_uri=${import.meta.env.VITE_REDIRECT_URL}`;
-            }}
-          >
-            <Symbol src={Kakao} alt="kakao" />
-            카카오 로그인
-          </button>
-        </div>
+        <button
+          className="rounded-small text-dark z-10 flex h-[50px] w-full cursor-pointer items-center justify-center bg-[#fee500] text-lg text-black transition-all duration-300 hover:bg-[#e8d102]"
+          onClick={() => {
+            window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${import.meta.env.VITE_KAKAO_REST_API_KEY}&redirect_uri=${import.meta.env.VITE_REDIRECT_URL}`;
+          }}
+        >
+          <Symbol src={Kakao} alt="kakao" />
+          카카오 로그인
+        </button>
+        <div className="rounded-normal absolute top-0 left-0 z-0 size-full backdrop-blur-sm" />
       </div>
     </div>
   );
