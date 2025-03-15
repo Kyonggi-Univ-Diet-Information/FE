48745ef3c1c4fd122e4a1c28af20d7ecc2e4e141
cho4u4o <cho4u4o@gmail.com>
2025-03-15 17:22:27 +0900

    design : fix container growing by applying height to parent component

--- src/widgets/home/ui/HomeContainer.tsx
+++ src/widgets/home/ui/HomeContainer.tsx
@@ -8,7 +8,7 @@ import { DormMenuBoard } from "~/widgets/home/ui";
 
 export default function HomeContainer() {
   return (
-    <div className="padding size-full">
+    <div className="padding h-[calc(100vh-4rem)] w-full">
       <div className="flex size-full flex-col gap-y-4 px-0 md:px-20">
         <HomeHeader />
         <DormMenuBoard />
