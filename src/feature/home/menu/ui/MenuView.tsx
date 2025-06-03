665bd98f4c12ecf7f5077cf13a474fd61a2ff4c3
cho4u4o <cho4u4o@gmail.com>
2025-06-03 17:34:54 +0900

    feat : add dummy data

--- src/feature/home/menu/ui/MenuView.tsx
+++ src/feature/home/menu/ui/MenuView.tsx
@@ -2,9 +2,10 @@ import React, { useEffect } from "react";
 import { useDateStore, useMenuStore } from "~/shared/store";
 import { cn, getDay, getDayKey } from "~/shared/utils";
 
-import { MenuItem, Time, WeeklyMenu } from "~/widgets/home/types";
+import { DailyMenu, MenuItem, Time, WeeklyMenu } from "~/widgets/home/types";
 import { RUN_TIME, setMenuData, TIME } from "~/widgets/home/model";
 import { useLoaderData } from "react-router-dom";
+import { Review } from "../../review/types";
 
 interface MenuViewProps {
   time: Time;
@@ -12,7 +13,6 @@ interface MenuViewProps {
 
 export default function MenuView({ time }: MenuViewProps) {
   const {
-    todayMenu,
     selectedMenu,
     setSelectedMenu,
     setSelectedMenuId,
@@ -22,6 +22,56 @@ export default function MenuView({ time }: MenuViewProps) {
   const { selectedDate } = useDateStore();
   const key = getDayKey(getDay(selectedDate));
   const data = useLoaderData();
+
+  const sampleReviews: Review[] = [];
+
+  const createMenuItem = (id: number, name: string): MenuItem => ({
+    id,
+    dietFoodDTO: {
+      id: 1000 + id,
+      name,
+      dietFoodReviews: sampleReviews,
+    },
+  });
+
+  const breakfastMenus = [
+    "닭가슴살 샐러드",
+    "오트밀과 바나나",
+    "계란 흰자 스크램블",
+    "고구마와 삶은 계란",
+    "그릭 요거트",
+    "토마토 치즈 오픈 샌드위치",
+    "연어 아보카도 토스트",
+    "닭죽",
+  ].map((name, i) => createMenuItem(i + 1, name));
+
+  const lunchMenus = [
+    "현미밥과 불고기",
+    "닭가슴살 김밥",
+    "두부 샐러드",
+    "우엉조림 도시락",
+    "닭가슴살 볶음밥",
+    "현미 리조또",
+    "쌈밥",
+    "연어 샐러드",
+  ].map((name, i) => createMenuItem(i + 100, name));
+
+  const dinnerMenus = [
+    "두부 스테이크",
+    "야채 스프",
+    "닭가슴살 구이",
+    "곤약국수",
+    "닭죽",
+    "샐러드볼",
+    "현미죽",
+    "계란찜과 나물",
+  ].map((name, i) => createMenuItem(i + 200, name));
+
+  const todayMenu: DailyMenu = {
+    BREAKFAST: { contents: breakfastMenus },
+    LUNCH: { contents: lunchMenus },
+    DINNER: { contents: dinnerMenus },
+  };
 
   useEffect(() => {
     if (data) {
@@ -36,7 +86,7 @@ export default function MenuView({ time }: MenuViewProps) {
       return <>주말에는 운영하지 않습니다.</>;
     if (!todayMenu) return <FetchFailed />;
     if (data && !todayMenu[TIME[time]]) return <>미운영</>;
-    if (data && todayMenu) {
+    if (todayMenu) {
       return (
         <>
           <p className="text-primary text-md-important">
