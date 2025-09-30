6184e76398ae1fdd9e4ff6007491e69fde22ac30
cho4u4o <cho4u4o@gmail.com>
2025-09-30 19:14:06 +0900

    feat : implement `Header`

--- apps/web/src/components/menu/Header.tsx
+++ apps/web/src/components/menu/Header.tsx
@@ -8,8 +8,12 @@ export default function Header() {
   const isLoggedIn = false;
 
   return (
-    <header className='flex h-14 w-full items-center justify-between bg-green-300 p-4'>
-      <span className='font-sans font-bold'>Let&apos;s eat, Kiryong!</span>
+    <header className='bg-border fixed top-0 z-50 flex h-14 w-full items-center justify-between p-8'>
+      <p className='flex items-baseline gap-2'>
+        <span className='font-brBold text-2xl font-bold'>기밥</span>
+        <span className='font-brRegular hidden md:block'>기룡아 밥먹자</span>
+      </p>
+
       {isLoggedIn ? (
         <Avatar>
           <AvatarImage src='https://github.com/shadcn.png' />
