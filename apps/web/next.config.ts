c98090777b6000c75ab58273c49ce2024902cc68
cho4u4o <cho4u4o@gmail.com>
2025-10-02 15:43:36 +0900

    chore : set next to show more detailed error in devmode

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+  ...(process.env.NODE_ENV === 'development' && {
+    onDemandEntries: {
+      maxInactiveAge: 25 * 1000,
+      pagesBufferLength: 2,
+    },
+  }),
 };
 
 export default nextConfig;
