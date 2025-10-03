8a11ca8a8ad74749ab14a873fbf62a878b4cb193
cho4u4o <cho4u4o@gmail.com>
2025-10-04 01:34:21 +0900

    feat : implement method fetching dorm menu

---
+++ apps/web/src/features/menu/services/fetchDormMenu.ts
@@ -0,0 +1,23 @@
+import { apiClient, ENDPOINT } from '@/lib/axios';
+import { DormDay, DormMenu, DormTime } from '@/types';
+
+interface FetchDormMenuRes {
+  result: {
+    [key in DormDay]: {
+      [key in DormTime]: {
+        id: number;
+        date: string;
+        time: DormTime;
+        contents: DormMenu[];
+      };
+    };
+  };
+}
+
+export const fetchDormMenu = async (): Promise<FetchDormMenuRes['result']> => {
+  const response = await apiClient.get<FetchDormMenuRes>({
+    request: ENDPOINT.DORM_MENU,
+  });
+
+  return response.data.result;
+};
--- apps/web/src/features/menu/services/index.ts
+++ apps/web/src/features/menu/services/index.ts
@@ -1,1 +1,2 @@
 export * from './fetchCampusMenu';
+export * from './fetchDormMenu';
