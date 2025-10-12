920748846ed01a3741d935d48c14e78120c4efb4
yummjin <cho4u4o@gmail.com>
2025-10-13 02:25:29 +0900

    fix : fix type annotations

--- apps/web/src/app/(subPages)/review/[foodId]/page.tsx
+++ apps/web/src/app/(subPages)/review/[foodId]/page.tsx
@@ -1,4 +1,4 @@
-// eslint-disable @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { notFound } from 'next/navigation';
 import Link from 'next/link';
 
