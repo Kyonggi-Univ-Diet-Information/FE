4d784c842c869f9d6851195fef2daec25a2041dd
yummjin <cho4u4o@gmail.com>
2025-10-12 16:44:47 +0900

    refactor : apply common layout at sub pages

--- apps/web/src/app/(subPages)/layout.tsx
+++ apps/web/src/app/(subPages)/layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 
-export default function ReviewLayout({ children }: { children: ReactNode }) {
+export default function SubPageLayout({ children }: { children: ReactNode }) {
   return (
     <div className='scrollbar-hide pb-26 absolute inset-0 flex flex-col gap-8 overflow-y-scroll p-4 pt-6'>
       {children}
