832d8620b0adf30af3a1fd5a2820f644ea8ac97a
cho4u4o <cho4u4o@gmail.com>
2025-10-16 00:09:33 +0900

    remove : remove unused component

--- apps/web/src/features/campus/components/index.ts
+++ apps/web/src/features/campus/components/index.ts
@@ -1,3 +1,2 @@
 export { default as CampusMenuCard } from './CampusMenuCard';
-export { default as CampusMenuView } from './CampusMenuView';
 export { default as CampusHeader } from './CampusHeader';
