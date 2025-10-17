dbeeecd53be3f60d5eadbdaed71b1baa39c510c0
cho4u4o <cho4u4o@gmail.com>
2025-10-17 21:23:52 +0900

    remove : remove default structures

--- apps/web/src/features/review/services/fetchMenuAverage.ts
+++
@@ -1,9 +0,0 @@
-import { apiClient, ENDPOINT } from '@/lib/axios';
-
-export const fetchMenuAverage = async (foodId: number): Promise<number> => {
-  const response = await apiClient.get<number>({
-    request: ENDPOINT.KS_REVIEW_AVERAGE_RATING + foodId,
-  });
-
-  return response.data;
-};
--- apps/web/src/types/date.ts
+++
@@ -1,3 +0,0 @@
-import { WEEKDAYS } from '@/lib/constants';
-
-export type Weekday = (typeof WEEKDAYS)[number];
