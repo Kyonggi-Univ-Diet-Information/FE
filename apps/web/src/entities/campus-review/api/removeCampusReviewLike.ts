4a60ea52c9cd341924e20bf31d03c382e398e4c7
cho4u4o <cho4u4o@gmail.com>
2025-10-18 15:00:16 +0900

    refactor : re-define campus like methods

---
+++ apps/web/src/entities/campus-review/api/removeCampusReviewLike.ts
@@ -0,0 +1,17 @@
+'use server';
+
+import { Http } from '@/shared/api/http';
+import { ENDPOINT, KEY } from '@/shared/config';
+import { revalidateTag } from 'next/cache';
+
+export const removeCampusReviewLike = async (reviewId: number) => {
+  await Http.del({
+    request: ENDPOINT.KS_REVIEW_LIKE.replace('{reviewId}', reviewId.toString()),
+    authorize: true,
+  }).catch(error => ({ success: false, error }));
+
+  revalidateTag(KEY.KS_REVIEW_LIKED);
+  revalidateTag(KEY.KS_REVIEW_LIKED_COUNT(reviewId));
+
+  return { success: true };
+};
