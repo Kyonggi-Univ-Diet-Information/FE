8c07c2848c9c8f3b697f2eef3b8eac76061f9556
yummjin <cho4u4o@gmail.com>
2025-10-21 12:54:26 +0900

    refactor : optimize fetchCampusMenuByRestaurant function with caching and sorting

--- apps/web/src/entities/campus-menu/api/fetchCampusMenuByRestaurant.ts
+++ apps/web/src/entities/campus-menu/api/fetchCampusMenuByRestaurant.ts
@@ -2,14 +2,17 @@ import { Http } from '@/shared/api/http';
 import { ENDPOINT } from '@/shared/config/endpoint';
 import type { SubRestaurant } from '../model/campusRestaurant';
 import type { CampusMenu } from '../model/campusMenu';
+import { cache } from 'react';
 
-export const fetchCampusMenuByRestaurant = async (
-  restaurantId: SubRestaurant,
-): Promise<CampusMenu[]> => {
-  const data = await Http.get<CampusMenu[]>({
-    request: ENDPOINT.CAMPUS_MENU_BY_RESTAURANT + restaurantId,
-    cache: 'force-cache',
-  });
+export const fetchCampusMenuByRestaurant = cache(
+  async (restaurantId: SubRestaurant): Promise<CampusMenu[]> => {
+    const data = await Http.get<CampusMenu[]>({
+      request: ENDPOINT.CAMPUS_MENU_BY_RESTAURANT + restaurantId,
+      cache: 'force-cache',
+    });
 
-  return data;
-};
+    const sorted = data.sort((a, b) => a.name.localeCompare(b.name));
+
+    return sorted;
+  },
+);
