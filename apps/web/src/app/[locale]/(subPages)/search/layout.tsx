54307e87ebc7f353cd840841480e6f187c7c0571
yummjin <cho4u4o@gmail.com>
2025-12-12 12:48:27 +0900

    design : adjust height of result body

--- apps/web/src/app/[locale]/(subPages)/search/layout.tsx
+++ apps/web/src/app/[locale]/(subPages)/search/layout.tsx
@@ -20,7 +20,7 @@ export default async function SearchLayout({
       >
         <SearchInput />
       </Section.Header>
-      <div className='scrollbar-hide absolute inset-0 bottom-20 top-36 overflow-y-scroll md:bottom-0 md:grid md:grid-cols-2 md:gap-4 md:px-4 md:pb-20'>
+      <div className='scrollbar-hide md:top-46 absolute inset-0 bottom-20 top-44 overflow-y-scroll md:bottom-0 md:grid md:grid-cols-2 md:gap-4 md:px-4 md:pb-20'>
         {children}
       </div>
     </>
