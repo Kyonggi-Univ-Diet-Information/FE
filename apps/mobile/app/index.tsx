55bad31dd62b86e0ee9df24a2d4e69ebde65c7b8
yummjin <cho4u4o@gmail.com>
2026-01-14 16:42:49 +0900

    feat : create mobile project for application

---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,66 @@
+import { StyleSheet, BackHandler, Platform } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+import { useEffect, useRef } from 'react';
+import { WebView } from 'react-native-webview';
+
+import { router } from 'expo-router';
+
+export default function Index() {
+  const webViewRef = useRef<WebView>(null);
+
+  useEffect(() => {
+    const backAction = () => {
+      if (router.canGoBack()) {
+        router.back();
+        return true;
+      }
+      if (webViewRef.current) {
+        webViewRef.current.goBack();
+        return true;
+      }
+
+      return false;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={[
+          styles.container,
+          {
+            paddingBottom: Platform.OS === 'ios' ? -24 : 0,
+          },
+        ]}
+      >
+        <WebView
+          ref={webViewRef}
+          source={{
+            uri:
+              process.env.EXPO_PUBLIC_WEB_URL ||
+              'https://new-kiryong-git-refactor-75-yujin-hans-projects.vercel.app/ko',
+          }}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          originWhitelist={['*']}
+          onError={error => console.error('WebView 에러:', error)}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+});
