a9458b792f971d4999fe52772b70a6261ee3b7ca
yummjin <cho4u4o@gmail.com>
2026-03-03 12:31:50 +0900

    refactor : remove 로딩중 text at search bar fallback

--- apps/web/src/app/(withHeader)/search/result/layout.tsx
+++ apps/web/src/app/(withHeader)/search/result/layout.tsx
@@ -9,10 +9,10 @@ export default async function SearchResultLayout({
 }) {
   return (
     <>
-      <Suspense fallback={<div>검색창 로딩 중...</div>}>
+      <Suspense fallback={<div></div>}>
         <SearchInput showFilter={true} />
       </Suspense>
-      <div className='scrollbar-hide top-34 absolute inset-0 overflow-y-scroll pb-20 md:grid md:h-fit md:grid-cols-2 md:gap-4 md:px-4'>
+      <div className='scrollbar-hide absolute inset-0 top-34 overflow-y-scroll pb-20 md:grid md:h-fit md:grid-cols-2 md:gap-4 md:px-4'>
         {children}
       </div>
     </>
